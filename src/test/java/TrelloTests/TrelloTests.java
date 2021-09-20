18067a18de63498cd7fcf109ad256e0f71d54410
Xena89 <quickusja89@hotmail.com>
2021-09-20 11:51:49 +0200

    add new board tests trello

--- src/test/java/TrelloTests/TrelloTests.java
+++ src/test/java/TrelloTests/TrelloTests.java
@@ -4,22 +4,30 @@ import TrelloPages.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class TrelloTests extends BeforeAndAfterActions {
     @Test
     @DisplayName("Тест логина и создания новой доски")
     void loginTrelloAndCreateNewBoardTest() {
         driver.get("https://trello.com/ru/login");
         driver.manage().window().maximize();
+
         new LoginPageTrello(driver)
+
                 .fillInputLogin("quickusja89@hotmail.com")
                 .loginWithAtlassianClick()
                 .fillInputPassword("100989quick")
                 .clickLoginButton();
+
         new BoardsPage(driver)
+
                 .createNewClick()
                 .createNewBoardClick()
                 .fillNewBoardTitle("New Board 100500")
                 .saveNewBoardButtonClick();
+
+        assertTrue(driver.getCurrentUrl().contains("/boards"));
 
     }
 }
